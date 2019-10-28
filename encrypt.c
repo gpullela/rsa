0209764048fed037e95d33abf51245df20b546a1
e101 <gourav.pullela@gmail.com>
2019-10-28 14:47:51 -0400

    first commit

---
+++ encrypt.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+#define N 96
+
+int isprime(int input){
+        if(input == 2){
+                return input;
+        }
+        else{
+                int i,j = 0;
+                for(i = 2; i < input; i++){
+                        if((i == (input-1))&&((input%i)!=0)){
+                                return input;
+                        }
+                        else if((input % i)==0){
+                                return 0;
+                        }else{
+                                continue;
+                        }
+                }
+                return 0;
+        }
+        return 0;
+}
+
+
+int primegen(int input){
+        int i = 0;
+        int x = 0;
+        for(i = input; i >= 2; i--){
+                x = isprime(i);
+                if(x==0){
+                        continue;
+                }
+                else if(x!=0){
+                        return i;
+                }
+        }
+        return 2;
+}
+
+int gcd(int x, int y){
+        int out = 1;
+        int i = 1;
+        for(i = 1; i <= x && i <= y; i++){
+                if((x%i)==0 && (y%i)==0){
+                        out = i;
+                }
+        }
+        return out;
+}
+
+int publickeygen(int p, int q){
+        int e = ((p-1)*(q-1))-1;
+        int i = 0;
+        for(i = 0; e >= 1; i++){
+                if(gcd(e,(p-1)*(q-1)) != 1){
+                        e--;
+                        continue;
+                }else{
+                        return e;
+                }
+        }
+        return e;
+}
+
+
+int main(){
+	int p = primegen(50);
+	int q = primegen(30);
+	int n = p*q;
+	int m = 0;
+	printf("enter a message: ");
+	scanf("%d\n",&m);
+	
+
+	return 0;
+
+}
