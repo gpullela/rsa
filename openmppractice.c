23b87e60c8389d6ac2f826ce7169d37469a29f8b
Gourav Venkat Pullela <gourav.pullela@gmail.com>
2019-11-28 18:35:06 -0500

    practicing openmp on silo

--- openmppractice.c
+++ openmppractice.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
 #include <omp.h>
 
+#define N 100
 
 int main(){
 
 
 	#pragma omp parallel
 	{
+		int i = 0;
+		int product = 1;
+		#pragma omp for
+		for(i = 1; i < N; i++){
+			product *= i;
+			printf("%d\n",product);
+		}
+
 		printf("\nHello World");
 	}
 
