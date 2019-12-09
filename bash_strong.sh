89132115baea6e6f8e2001af75ab108c33668e9d
Gourav Pullela <gourav.pullela@gmail.com>
2019-12-09 16:54:27 -0500

    completed weak and strong scaling on home computer

--- bash_strong.sh
+++ bash_strong.sh
@@ -1,6 +1,21 @@
 #!/bin/bash
 
 echo "RSA ENCRYPTION"
+echo "Commencing Data Collection"
+echo "Run 1"
+export OMP_NUM_THREADS=1
+./a.out robert_frost1.txt >> results_strong.dat
+
+export OMP_NUM_THREADS=2
+./a.out robert_frost1.txt >> results_strong.dat
+
+export OMP_NUM_THREADS=4
+./a.out robert_frost1.txt >> results_strong.dat
+
+export OMP_NUM_THREADS=8
+./a.out robert_frost1.txt >> results_strong.dat
+
+echo "Run 2"
 
 export OMP_NUM_THREADS=1
 ./a.out robert_frost1.txt >> results_strong.dat
@@ -14,4 +29,50 @@ export OMP_NUM_THREADS=4
 export OMP_NUM_THREADS=8
 ./a.out robert_frost1.txt >> results_strong.dat
 
+
+echo "Run 3"
+
+export OMP_NUM_THREADS=1
+./a.out robert_frost1.txt >> results_strong.dat
+
+export OMP_NUM_THREADS=2
+./a.out robert_frost1.txt >> results_strong.dat
+
+export OMP_NUM_THREADS=4
+./a.out robert_frost1.txt >> results_strong.dat
+
+export OMP_NUM_THREADS=8
+./a.out robert_frost1.txt >> results_strong.dat
+
+
+echo "Run 4"
+
+export OMP_NUM_THREADS=1
+./a.out robert_frost1.txt >> results_strong.dat
+
+export OMP_NUM_THREADS=2
+./a.out robert_frost1.txt >> results_strong.dat
+
+export OMP_NUM_THREADS=4
+./a.out robert_frost1.txt >> results_strong.dat
+
+export OMP_NUM_THREADS=8
+./a.out robert_frost1.txt >> results_strong.dat
+
+
+echo "Run 5"
+
+export OMP_NUM_THREADS=1
+./a.out robert_frost1.txt >> results_strong.dat
+
+export OMP_NUM_THREADS=2
+./a.out robert_frost1.txt >> results_strong.dat
+
+export OMP_NUM_THREADS=4
+./a.out robert_frost1.txt >> results_strong.dat
+
+export OMP_NUM_THREADS=8
+./a.out robert_frost1.txt >> results_strong.dat
+
+echo "Completed Data Collection"
 echo "COMPLETED SIMULATION"
--- bash_weak.sh
+++ bash_weak.sh
@@ -1,17 +1,80 @@
 #!/bin/bash
 
 echo "RSA ENCRYPTION"
+echo "Commencing Data Collection"
+echo "Run 1"
 
 export OMP_NUM_THREADS=1
-./a.out robert_frost1.txt >> results_strong.dat
+./a.out robert_frost1.txt >> results_weak.dat
 
 export OMP_NUM_THREADS=2
-./a.out robert_frost2.txt >> results_strong.dat
+./a.out robert_frost2.txt >> results_weak.dat
 
 export OMP_NUM_THREADS=4
-./a.out robert_frost4.txt >> results_strong.dat
+./a.out robert_frost4.txt >> results_weak.dat
 
 export OMP_NUM_THREADS=8
-./a.out robert_frost8.txt >> results_strong.dat
+./a.out robert_frost8.txt >> results_weak.dat
 
+
+echo "Run 2"
+
+export OMP_NUM_THREADS=1
+./a.out robert_frost1.txt >> results_weak.dat
+
+export OMP_NUM_THREADS=2
+./a.out robert_frost2.txt >> results_weak.dat
+
+export OMP_NUM_THREADS=4
+./a.out robert_frost4.txt >> results_weak.dat
+
+export OMP_NUM_THREADS=8
+./a.out robert_frost8.txt >> results_weak.dat
+
+
+echo "Run 3"
+
+export OMP_NUM_THREADS=1
+./a.out robert_frost1.txt >> results_weak.dat
+
+export OMP_NUM_THREADS=2
+./a.out robert_frost2.txt >> results_weak.dat
+
+export OMP_NUM_THREADS=4
+./a.out robert_frost4.txt >> results_weak.dat
+
+export OMP_NUM_THREADS=8
+./a.out robert_frost8.txt >> results_weak.dat
+
+
+echo "Run 4"
+
+export OMP_NUM_THREADS=1
+./a.out robert_frost1.txt >> results_weak.dat
+
+export OMP_NUM_THREADS=2
+./a.out robert_frost2.txt >> results_weak.dat
+
+export OMP_NUM_THREADS=4
+./a.out robert_frost4.txt >> results_weak.dat
+
+export OMP_NUM_THREADS=8
+./a.out robert_frost8.txt >> results_weak.dat
+
+
+echo "Run 5"
+
+export OMP_NUM_THREADS=1
+./a.out robert_frost1.txt >> results_weak.dat
+
+export OMP_NUM_THREADS=2
+./a.out robert_frost2.txt >> results_weak.dat
+
+export OMP_NUM_THREADS=4
+./a.out robert_frost4.txt >> results_weak.dat
+
+export OMP_NUM_THREADS=8
+./a.out robert_frost8.txt >> results_weak.dat
+
+echo "Completed Data Collection"
 echo "COMPLETED SIMULATION"
